93fc56c2a30ae6e755d9b331470d37242c6df319
Sergey Khudoyaro <shudoyarov@gmail.com>
2021-03-22 18:55:13 +0300

    4+

--- 1.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "MyTech/2.Maps/task1"
-
-func main() {
-	task1.Run()
-}
--- 2.Maps/task3_hash1/task3_hash1.go
+++ 2.Maps/task3_hash1/task3_hash1.go
@@ -1,11 +1,29 @@
 package task3_hash1
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 //Add
 //Delete
 //InHash
 //PrintValues
+
+func Run() {
+	m := make(map[string]int64)
+	FillMap(m)
+
+	fmt.Println(GetList(m))
+}
+
+func FillMap(m map[string]int64) map[string]int64 {
+	Add(m, "American Express", 378282246310005)
+	Add(m, "Master Card", 5105105105105100)
+	Add(m, "Visa", 4222222222222)
+	Add(m, "JCB", 3530111333300000)
+	return m
+}
 
 func Add(m map[string]int64, key string, v int64) {
 	m[key] = v
@@ -23,10 +41,7 @@ func InHash(m map[string]int64, key string) bool {
 func GetList(m map[string]int64) string {
 	var str string
 	for key, v := range m {
-		str = "[" + key + "]" + v + "\n"
+		str += "[" + key + "]" + strconv.Itoa(int(v)) + "\n"
 	}
-}
-
-func strtoint() {
-
+	return str
 }
