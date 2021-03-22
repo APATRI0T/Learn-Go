da5ee83601ca67d7b64e03651d58d86b73c57d7c
Sergey Khudoyaro <shudoyarov@gmail.com>
2021-03-22 08:11:25 +0300

    Добавил структуру файлов, чтоб можно было легко добавлять новые задачи

--- 1.go
+++ 1.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
-}
+import "MyTech/2.Maps/task1"
 
 func main() {
-	hypot := func(x, y float64) float64 {
-		return math.Sqrt(x*x + y*y)
-	}
-	fmt.Println(hypot(5, 12))
-
-	fmt.Println(compute(hypot))
-	fmt.Println(compute(math.Pow))
+	task1.Run()
 }
